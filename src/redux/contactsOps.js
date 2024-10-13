919f629f445fdf004b8f88bb890fafa4ffd25797
karikate <k.sarancha96@gmail.com>
2024-10-13 21:42:35 +0300

    add thunk

---
+++ src/redux/contactsOps.js
@@ -0,0 +1,43 @@
+// fetchContacts - одержання масиву контактів (метод GET) запитом. Базовий тип екшену це рядок "contacts/fetchAll".
+// addContact - додавання нового контакту (метод POST). Базовий тип екшену це рядок "contacts/addContact".
+// deleteContact - видалення контакту по ID (метод DELETE). Базовий тип екшену це рядок "contacts/deleteContact".
+
+import { asyncThunkCreator } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = `https://670c0e727e5a228ec1cf6f7a.mockapi.io`;
+
+export const fetchContactsThunk = asyncThunkCreator(
+  `contacts / fetchAll`,
+  async (_, thunkApi) => {
+    try {
+      const { data } = await axios.get(`/contacts`);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContactThunk = asyncThunkCreator(
+  `contacts / addContact`,
+  async (body, thunkApi) => {
+    try {
+      const { data } = await axios.post(`/contacts`, body);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+export const deleteContactThunk = asyncThunkCreator(
+  `contacts / deleteContact`,
+  async (id, thunkApi) => {
+    try {
+      const { data } = await axios.delete(`/contacts/${id}`);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -7,6 +7,8 @@ const initialState = {
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ],
+  loading: false,
+  error: null,
 };
 
 const contactSlice = createSlice({
