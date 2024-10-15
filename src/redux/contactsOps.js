cab0b0d65c62e2a5a79d7913609132c2dca48022
karikate <k.sarancha96@gmail.com>
2024-10-15 15:38:59 +0300

    fix note

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,3 @@
-// fetchContacts - одержання масиву контактів (метод GET) запитом. Базовий тип екшену це рядок "contacts/fetchAll".
-// addContact - додавання нового контакту (метод POST). Базовий тип екшену це рядок "contacts/addContact".
-// deleteContact - видалення контакту по ID (метод DELETE). Базовий тип екшену це рядок "contacts/deleteContact".
-
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
